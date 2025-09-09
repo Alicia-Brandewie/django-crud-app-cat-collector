3527ea90b94f25a8ac5eb84634e37419192f1b89
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-09-09 12:07:00 -0500

    cat detail pages added & dynamic URLs implemented

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,11 @@
 from django.shortcuts import render
 
+from .models import Cat
+
 # from django.http import HttpResponse
 
 #mock data for later when we hook up to DB
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
 
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
 
 # Create your views here.
 def home(request):
@@ -25,4 +15,9 @@ def about(request):
     return render(request, 'about.html')
 
 def cat_index(request):
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})

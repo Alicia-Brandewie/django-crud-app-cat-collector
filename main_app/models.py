7e0d014cc6ba736f5f351c298302f5290f19f748
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-09-11 12:32:33 -0500

    toys UI functioning on cat detail page

--- main_app/models.py
+++ main_app/models.py
@@ -7,13 +7,23 @@ MEALS = (
     ('D', 'Dinner')
 )
 
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('toy-detail', kwargs={'pk': self.id})
+    
 # Create your models here.
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy) #also have to move DEF TOY above Cat Model
 
     def __str__(self):
         return self.name
@@ -40,12 +50,3 @@ class Feeding(models.Model):
     class Meta:
         ordering = ['-date']  # This line makes the newest feedings appear first
 
-class Toy(models.Model):
-    name = models.CharField(max_length=50)
-    color = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse('toy-detail', kwargs={'pk': self.id})
